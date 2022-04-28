10b5b2ed79eb4e1b168d1d15a4a97417aeadd6a6
chy <781288772@qq.com>
2022-04-28 21:52:34 +0800

    测试接入微信

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -23,7 +23,7 @@
 			<view class="title">
 				支付方式
 			</view>
-			<!-- 	<view class="flex_bt" @click="handleSelectPayType(0)" style="margin-top: 18px;">
+				<view class="flex_bt" @click="handleSelectPayType(0)" style="margin-top: 18px;">
 				<view class="flex_st">
 					<image src="../../static/wx.png" mode="" style="width: 28px;height: 28px;"></image>
 					<text style="margin-left: 10px;font-size: 20px;">微信</text>
@@ -31,10 +31,10 @@
 				<view>
 					<image src="../../static/selected.png" mode="" style="width: 16px;height:16px;"
 						v-if="payType==0"></image>
-					<image src="../../static/unselected.png" mode="" style="width: 0.8rem;height: 0.8rem" v-else>
+					<image src="../../static/unselected.png" mode="" style="width: 16px;height: 16px" v-else>
 					</image>
 				</view>
-			</view> -->
+			</view>
 			<!-- 支付宝 -->
 			<view class="flex_bt" style="margin-top: 18px;" @click="handleSelectPayType(1)">
 				<view class="flex_st">
@@ -211,7 +211,7 @@
 							document.forms[0].submit();
 
 						} else {
-							window.location.href = res.data.result.h5_url;
+							// window.location.href = res.data.result.h5_url;
 						}
 
 
